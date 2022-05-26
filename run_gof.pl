d4c3eea0d92a0910812727df767ef5b4221b12fb
Wenqiang Gu <goowenq@gmail.com>
2022-05-26 13:40:08 -0400

    cfg for model validation

--- run_gof.pl
+++ run_gof.pl
@@ -8,16 +8,16 @@ system("./convert_histo.pl");
 system("./bin/stat_pred_cov_matrix -r0 &");
 
 #Det sys
-system("./run_det_sys.pl");
+# system("./run_det_sys.pl");
 
 #data statistical uncertainties
-system("./bin/stat_cov_matrix -r0 &");
+# system("./bin/stat_cov_matrix -r0 &");
 
 #Flux sys, GEANT4 1-->17
 system("./run_xf_sys.pl");
 
 # MC stat ... 
-system("./bin/merge_hist -r0 -l0 -e2 > ./mc_stat/xs_tot.log");
+system("./bin/merge_hist -r0 -l0 -e2 > ./mc_stat/0.log");
 
 #central files ...
 system("./bin/merge_hist -r0 -l0");
